98bd1457d8d111538e0b9c8fdf8d0fe9bf11fa6c
Denny Mathew <devdenmathew@gmail.com>
2018-09-23 16:02:51 +0530

    Initial Commit

---
+++ SquareDebts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareDebts
+//
+//  Created by Denny Mathew on 23/09/18.
+//  Copyright © 2018 Densigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
